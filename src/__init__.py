ddd2d69e39fe3d598623a4b106f71dcd7374175a
Clayton Bytyci <claytonresulaj@hotmail.com>
2026-02-08 12:19:26 +0000

    Initial Classes

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from .ensemble import (
+    KLMultiplicativeWeights,
+    KLOnlineGradientDescent,
+    MultiplicativeWeights,
+    OnlineGradientDescent,
+)
+
+__all__ = [
+    "OnlineGradientDescent",
+    "MultiplicativeWeights",
+    "KLOnlineGradientDescent",
+    "KLMultiplicativeWeights",
+]
